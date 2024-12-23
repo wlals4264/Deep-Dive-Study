3c4be58cd7bb09746e0d0975a980a59391915d1e
bluemin <zeem_m2@naver.com>
2024-12-23 19:18:26 +0900

    27 배열(2) - 배열 메서드

--- practice.js
+++ practice.js
@@ -1,29 +1,21 @@
-const counter = (function () {
-  // 카운트 상태를 유지하기 위한 자유 변수
-  let counter = 0;
+function solution(s = 1110000101) {
+  // 문자열을 돌면서 1이 나오면 해당 인덱스를 저장하는 배열을 만든다.
+  // 1110000101
+  // 012    7 9
+  // 만약 배열의 length가 6보다 작거나 같고
+  // 1. 배열의 0번째 값과 2번째 값의 차가 2보다 작거나 같으면 일단 한 손 연주 가능
+  // 2. 배열의 3번째 값과 배열의 마지막 값의 차가 2보다 작거나 같으면 두 손 연주 가능
+  // 1번과 2번 모두 만족해야 양손 연주가능한 곡이다 "YES"출력
+  // 아니면 "NO" 출력
+  const pushKeysArr = [];
 
-  // 함수를 인수로 전달 받는 클로저를 반환
-  return function (aux) {
-    // 인수로 전달받은 보조 함수에 상태 변경을 위임한다.
-    counter = aux(counter);
-    return counter;
-  };
-})();
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === 1) {
+      pushKeysArr.push(i);
+    }
+  }
 
-// 보조 함수
-function increase(n) {
-  return ++n;
+  console.log(pushKeysArr);
 }
 
-// 보조 함수
-function decrease(n) {
-  return --n;
-}
-
-// 보조 함수를 전달하여 호출
-console.log(counter(increase)); // 1
-console.log(counter(increase)); // 2
-
-// 자유 변수를 공유한다.
-console.log(counter(decrease)); // 1
-console.log(counter(decrease)); // 0
+solution();
